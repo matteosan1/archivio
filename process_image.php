3e8b324005057450415693a9ec371a3ebfd69be1
Matteo Sani <matteosan1@gmail.com>
2020-02-13 15:28:56 +0100

    Add files via upload

--- process_image.php
+++ process_image.php
@@ -11,34 +11,31 @@ $tagl2 = "aceto"; //$sqlite->querySingle("SELECT name FROM tags WHERE id=".$_POS
 $list_of_tags = array("pippo22", "pluto"); //explode(",", $_POST['list_of_tags']);
 $img_ext = array('tif', 'tiff', 'jpeg', 'jpg');
 
-$target_directory = '/var/www/myupload/';
+$target_directory = '/var/www/myupload/'.$tagl1.'/'.$tagl2.'/';
 $maxsize = 200000000;
 
 //$ext_ok = array('csv', 'pdf', 'tif', 'tiff', 'jpeg', 'jpg', 'png', 'doc', 'docx');
-//if (isset($_POST['submit'])) 
-{
+if (isset($_POST['submit'])) {
+	
 	if (!isset($_FILES['userfile'])) { // || !is_uploaded_file($_FILES['userfile']['tmp_name'])) {
                 echo 'No file uploaded...';
                 exit;
         }
 
 	$countfiles = count($_FILES['userfile']['name']);
-	echo "$countfiles";
 	for ($i=0; $i<$countfiles; $i++) {
 		$filename = $_FILES['userfile']['name'][$i];
-		$ext = end(explode(".", $filename));
-		echo $filename . "  ". $ext;
-		//print_r ($img_ext);
-		if ($ext == "zip") {
+		$ext = explode(".", $filename);
+		if (end($ext) == "zip") {
 			if (basicCheckOnFile($i)) {
 				processZip($_FILES['userfile']['tmp_name'][$i]);
 			}
-		} elseif (in_array($ext, $img_ext)) {
+		} elseif (in_array(end($ext), $img_ext)) {
 			if (basicCheckOnFile($i)) {
 				processImage($_FILES['userfile']['tmp_name'][$i], $_FILES['userfile']['name'][$i]);
 			}
 	        } else {		
-			echo "File type ".$ext." is not allowed";
+			echo "File type ".end($ext)." is not allowed";
 			exit;
 		}
     	}	
@@ -70,14 +67,13 @@ function addEXIF($filename) {
 }
 
 function processZip($zipfilename) {
-	global $img_ext;
+	global $img_ext, $target_directory;
 
 	$zip = new ZipArchive;
         $res = $zip->open($zipfilename);
 	
 	if ($res === TRUE) {
         	$zip->extractTo(dirname($zipfilename));
-                //$zip->close();
                 echo 'Unziped!';
         } else {
                 echo "Unzip of ".$filename." (".$zipfilename.") failed!";
@@ -85,12 +81,11 @@ function processZip($zipfilename) {
 	}
 
 	for($i = 0; $i < $zip->numFiles; $i++) {   
-		echo "$i";
 		$fz = $zip->getNameIndex($i);
-		echo $fz;
- 		$ext = end(explode(".", $fz));
-		if (in_array($ext, $img_ext)) {
-	       		processImage($fz, $fz);
+ 		$ext = explode(".", $fz);
+		if (in_array(end($ext), $img_ext)) {
+			rename('/var/www/myupload/'.$fz, $target_directory . $fz);
+			addEXIF($target_directory . $fz);
 		}
 	}
 	$zip->close();
@@ -100,16 +95,18 @@ function processImage($userfile_tmp, $userfile_name) {
 
 	global $target_directory;
 
-    	//$userfile_tmp = $_FILES['userfile']['tmp_name'][$i];
-	//$userfile_name = $_FILES['userfile']['name'][$i];
+	if (! file_exists($target_directory)) {
+		mkdir($target_directory, 0777, TRUE);
+	}
 
-	addEXIF($userfile_tmp);
     	if (move_uploaded_file($userfile_tmp, $target_directory . $userfile_name)) {
     		echo 'File '.$userfile_name.' successfully uploaded.';
 	    	//exec(dirname(__FILE__) . '/myscript.sh');
 		//shell_exec
-    	} else {
+	} else {
     		echo 'Upload of '.$userfile_name.' failed';
-    	}
+	}
+
+	addEXIF($target_directory . $userfile_name);
 }
 ?>
