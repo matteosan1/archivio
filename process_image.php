81cda10ef1e7ae9273262b536f99d0a2a2f0817e
Matteo Sani <matteosan1@gmail.com>
2020-02-13 12:38:42 +0100

    Add files via upload

--- process_image.php
+++ process_image.php
@@ -4,10 +4,11 @@ ini_set('display_errors', 1); // SET IT TO 0 ON A LIVE SERVER !!!
 ini_set('display_startup_errors', 1); // SET IT TO 0 ON A LIVE SERVER !!!
 
 include ("db.php");
+require_once("class/exif.php");
 
-$tagl1 = $sqlite->querySingle("SELECT name FROM tags WHERE id=".$_POST['tagl1'].";", FALSE);
-$tagl2 = $sqlite->querySingle("SELECT name FROM tags WHERE id=".$_POST['tagl2'].";", FALSE);
-$list_of_tags = explode(",", $_POST['list_of_tags']);
+$tagl1 = "palio"; //$sqlite->querySingle("SELECT name FROM tags WHERE id=".$_POST['tagl1'].";", FALSE);
+$tagl2 = "aceto"; //$sqlite->querySingle("SELECT name FROM tags WHERE id=".$_POST['tagl2'].";", FALSE);
+$list_of_tags = array("pippo22", "pluto"); //explode(",", $_POST['list_of_tags']);
 $img_ext = array('tif', 'tiff', 'jpeg', 'jpg');
 
 $target_directory = '/var/www/myupload/';
@@ -16,68 +17,67 @@ $maxsize = 200000000;
 //$ext_ok = array('csv', 'pdf', 'tif', 'tiff', 'jpeg', 'jpg', 'png', 'doc', 'docx');
 //if (isset($_POST['submit'])) 
 {
-	if (!isset($_FILES['userfile']) || !is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+	if (!isset($_FILES['userfile'])) { // || !is_uploaded_file($_FILES['userfile']['tmp_name'])) {
                 echo 'No file uploaded...';
                 exit;
         }
 
 	$countfiles = count($_FILES['userfile']['name']);
-
+	echo "$countfiles";
 	for ($i=0; $i<$countfiles; $i++) {
 		$filename = $_FILES['userfile']['name'][$i];
-	   	$ext = end(explode(".", $filename));
-		if (end($ext) == ".zip") {
-			if (basicCheckOnFile($i) {
-				//$_zipfilename = $_FILES['userfile']['tmp_name'][$i];
+		$ext = end(explode(".", $filename));
+		echo $filename . "  ". $ext;
+		//print_r ($img_ext);
+		if ($ext == "zip") {
+			if (basicCheckOnFile($i)) {
 				processZip($_FILES['userfile']['tmp_name'][$i]);
 			}
-			//$zip = new ZipArchive;
-			//$res = $zip->open($zipfilename);
-			//if ($res === TRUE) {
- 			//	$zip->extractTo(dirname($zipfilename));
- 			//	$zip->close();
- 			//	echo 'Unziped!';
-			//} else {
- 			//	echo "Unzip of ".$filename." (".$zipfilename.") failed!";
-			//	exit;	
-			//}
-			// Processare file per file unzippati
-		} else if (!in_array($ext, $ext_img)) {
-			if (basicCheckOnFile($i) {
+		} elseif (in_array($ext, $img_ext)) {
+			if (basicCheckOnFile($i)) {
 				processImage($_FILES['userfile']['tmp_name'][$i], $_FILES['userfile']['name'][$i]);
 			}
 	        } else {		
 			echo "File type ".$ext." is not allowed";
-        		exit;
+			exit;
+		}
     	}	
 }	
 
 function basicCheckOnFile($i) {
-	if ($_FILE['userfile']['size'][$i] > $maxsize) {
+	global $maxsize, $target_directory;
+
+	if ($_FILES['userfile']['size'][$i] > $maxsize) {
                 echo "File ".$_FILE['userfile']['name'][$i]." is too big !";
                	return FALSE;
         }
 	
 	$target_file = $target_directory.$_FILES['userfile']['name'][$i];
 	if (file_exists($target_file)) {
-                echo 'File '.$_FILE['userfile']['name'][$i].'already exists.';
+                echo 'File '.$_FILES['userfile']['name'][$i].'already exists.';
                 return FALSE;
 	}
 
 	return TRUE;
 }
 
+function addEXIF($filename) {
+	global $list_of_tags, $tagl1, $tagl2;
 
-function addEXIF() {
+	$objIPTC = new IPTC($filename);
+	$additional_tags = implode(" ", $list_of_tags);
+	$objIPTC->setValue(IPTC_KEYWORDS, $additional_tags." ".$tagl1." ".$tagl2);
 }
 
-function processZip() {
+function processZip($zipfilename) {
+	global $img_ext;
+
 	$zip = new ZipArchive;
         $res = $zip->open($zipfilename);
-
+	
 	if ($res === TRUE) {
         	$zip->extractTo(dirname($zipfilename));
-                $zip->close();
+                //$zip->close();
                 echo 'Unziped!';
         } else {
                 echo "Unzip of ".$filename." (".$zipfilename.") failed!";
@@ -85,21 +85,25 @@ function processZip() {
 	}
 
 	for($i = 0; $i < $zip->numFiles; $i++) {   
-        	$fz = $zip->getNameIndex($i);
+		echo "$i";
+		$fz = $zip->getNameIndex($i);
+		echo $fz;
  		$ext = end(explode(".", $fz));
-		if (!in_array($ext, $ext_img)) {
-        		processImage($fz, $fz);
+		if (in_array($ext, $img_ext)) {
+	       		processImage($fz, $fz);
 		}
 	}
+	$zip->close();
 }
 
 function processImage($userfile_tmp, $userfile_name) {
-    
+
+	global $target_directory;
+
     	//$userfile_tmp = $_FILES['userfile']['tmp_name'][$i];
 	//$userfile_name = $_FILES['userfile']['name'][$i];
 
 	addEXIF($userfile_tmp);
-
     	if (move_uploaded_file($userfile_tmp, $target_directory . $userfile_name)) {
     		echo 'File '.$userfile_name.' successfully uploaded.';
 	    	//exec(dirname(__FILE__) . '/myscript.sh');
